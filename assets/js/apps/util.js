c76623162a9c128c1e027aaebb72a4c9ed60fde4
Paul Citarella <pc@paulcitarella.com>
2015-10-28 08:36:02 -0400

    Fix for unknown provider error in prod mode

--- assets/js/apps/util.js
+++ assets/js/apps/util.js
@@ -1,9 +1,9 @@
 module.exports.jstCache = function($provide) {
-  $provide.decorator('$templateCache', function($delegate) {
+  $provide.decorator('$templateCache', ['$delegate', function($delegate) {
       var get = $delegate.get;
       $delegate.get = function(key) {
           return window.JST[key] || get(key);
       };
       return $delegate;
-  });
+  }]);
 };
