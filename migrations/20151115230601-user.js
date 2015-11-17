01296b6eb0bcfc8dededc54361371989473b1e0c
Paul Citarella <pc@paulcitarella.com>
2015-11-17 08:29:28 -0500

    Migration for user

---
+++ migrations/20151115230601-user.js
@@ -0,0 +1,54 @@
+var dbm = global.dbm || require('db-migrate');
+var type = dbm.dataType;
+var Promise = require('bluebird');
+
+exports.up = function(db, callback) {
+  var adb = Promise.promisifyAll(db);
+  adb.createTableAsync('person', {
+    id: {
+      type: 'int',
+      primaryKey: true,
+      autoIncrement: true
+    },
+    username: {
+      type: 'text',
+      notNull:true,
+      unique: true
+    },
+    email: {
+      type: 'text',
+      notNull:true,
+      unique: true
+    },
+    firstname: {
+      type: 'text',
+      notNull:true
+    },
+    lastname: {
+      type: 'text',
+      notNull:true
+    },
+    createdAt: 'timestamptz',
+    updatedAt: 'timestamptz'
+
+  }).then(function() {
+    return [
+      adb.addIndexAsync('person', 'person_email', ['email'], true),
+      adb.addIndexAsync('person', 'person_username', ['username'], true)
+    ];
+  }).all()
+
+  .finally(callback);
+};
+
+exports.down = function(db, callback) {
+  var adb = Promise.promisifyAll(db);
+  Promise.all([
+    adb.removeIndexAsync('person', 'person_email'),
+    adb.removeIndexAsync('person', 'person_username')
+
+  ]).then(function() {
+    return adb.dropTableAsync('person', {});
+
+  }).finally(callback);
+};
